367380342f8109c55523dd3f81044e119bdc4d3d
Dracovolodeus <bananaed12@gmail.com>
2025-04-08 00:00:37 +0700

    Increasing the timeouts in hypridle

--- users/meowta/home-manager/modules/hypridle/default.nix
+++ users/meowta/home-manager/modules/hypridle/default.nix
@@ -10,31 +10,31 @@
 
       listener = [
         {
-          timeout = 150;
+          timeout = 180;
           on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0";
           on-resume = "brightnessctl -rd rgb:kbd_backlight";
         }
 
         {
-          timeout = 300;
+          timeout = 900;
+          on-timeout = "brightnessctl -s set 10";
+          on-resume = "brightnessctl -r";
+        }
+
+        {
+          timeout = 1185;
           on-timeout = "loginctl lock-session";
         }
 
         {
-          timeout = 330;
+          timeout = 1215;
           on-timeout = "hyprctl dispatch dpms off";
           on-resume = "hyprctl dispatch dpms on";
         }
 
         {
-          timeout = 1800;
+          timeout = 2700;
           on-timeout = "systemctl suspend";
-        }
-
-        {
-          timeout = 150;
-          on-timeout = "brightnessctl -s set 10";
-          on-resume = "brightnessctl -r";
         }
       ];
     };
