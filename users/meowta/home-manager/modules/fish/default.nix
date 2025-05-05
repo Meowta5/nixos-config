93a30a5a5553b96bc26b5a7acbf7bbfdf53532bc
Dracovolodeus <bananaed12@gmail.com>
2025-05-05 20:20:14 +0700

    delete trash fish commands

--- users/meowta/home-manager/modules/fish/default.nix
+++ users/meowta/home-manager/modules/fish/default.nix
@@ -6,8 +6,6 @@
       cls = "clear";
       n = "nvim";
       g = "git";
-      pcv3 = "python3 -m venv venv";
-      pav3 = ". ./venv/bin/activate.fish";
       snc = "cd /nixos-config";
       suhmd = "cd /nixos-config/users/$USER/home-manager";
       swuhm = "git add .; home-manager switch --flake ./#$USER";
